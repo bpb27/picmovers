f50f6fb437e7f06f91e2a9625f885046b75f46ce
Brendan Brown <brendanbrown27@gmail.com>
2017-07-09 14:11:20 -0400

    first commit

---
+++ videos.js
@@ -0,0 +1,144 @@
+var videos = [ //don't touch this line
+
+{
+    "link": "https://vimeo.com/223193264",
+    "image": "a_scholars_journey.png",
+    "title": "A Scholar's Journey"
+},
+{
+    "link": "https://vimeo.com/134523001",
+    "image": "aging_homelessness.jpeg",
+    "title": "Aging Homeless"
+},
+{
+    "link": "https://youtu.be/SbGpQ9IM_dU",
+    "image": "andrew_watt_and_post_malone_burning_man.jpeg",
+    "title": "Andrew Watt and Post Malone - Burning Man"
+},
+{
+    "link": "https://vimeo.com/183758023",
+    "image": "bairons_story.jpeg",
+    "title": "Bairon's Story"
+},
+{
+    "link": "https://vimeo.com/215274468",
+    "image": "casa_laguna_hotel.png",
+    "title": "Casa Laguna Hotel"
+},
+{
+    "link": "https://vimeo.com/183715346",
+    "image": "chloes_story.jpeg",
+    "title": "Chloe's Story"
+},
+{
+    "link": "https://vimeo.com/172836775",
+    "image": "destinys_story.jpeg",
+    "title": "Destiny’s Story"
+},
+{
+    "link": "https://vimeo.com/170905990",
+    "image": "first_date_from_hell.jpeg",
+    "title": "First Date from Hell"
+},
+{
+    "link": "https://vimeo.com/130369867",
+    "image": "homeless_youth.jpeg",
+    "title": "Homeless Youth"
+},
+{
+    "link": "https://vimeo.com/132995652",
+    "image": "how_to_pasta.jpeg",
+    "title": "How to Pasta"
+},
+{
+    "link": "https://vimeo.com/172182876",
+    "image": "jajas_story.jpeg",
+    "title": "Ja-Ja's Story"
+},
+{
+    "link": "https://www.youtube.com/watch?v=yJ5Q_Hy2O1U",
+    "image": "janine_dont_love_me.jpeg",
+    "title": "Janine - Don't Love Me"
+},
+{
+    "link": "https://vimeo.com/124585060",
+    "image": "jonwayne_minerals_and_gems.jpeg",
+    "title": "Jonwayne - Minerals and Gems"
+},
+{
+    "link": "https://vimeo.com/105971446",
+    "image": "kiva_zip.jpeg",
+    "title": "Kiva Zip"
+},
+{
+    "link": "https://www.youtube.com/watch?v=MKe2AU-7XJQ",
+    "image": "naked_man_locks_himself_out.jpeg",
+    "title": "Naked man locks himself out of hotel room"
+},
+{
+    "link": "https://vimeo.com/91400409",
+    "image": "norman_silverman.jpeg",
+    "title": "Norman Silverman"
+},
+{
+    "link": "https://vimeo.com/100456001",
+    "image": "old_man_saxon_soup.jpeg",
+    "title": "Old Man Saxon - SOUP"
+},
+{
+    "link": "https://vimeo.com/76600072",
+    "image": "on_point.jpeg",
+    "title": "On Point"
+},
+{
+    "link": "https://vimeo.com/180961328",
+    "image": "on_the_wedding_party_set.jpeg",
+    "title": "On The Wedding Party Set"
+},
+{
+    "link": "https://vimeo.com/135148780",
+    "image": "parental_substance_abuse.jpeg",
+    "title": "Parental Substance Abuse"
+},
+{
+    "link": "https://vimeo.com/96759872",
+    "image": "quientin_and_lisa.jpeg",
+    "title": "Quentin and Lisa at the Grocery Store"
+},
+{
+    "link": "https://vimeo.com/181891849",
+    "image": "the_great_breakup_contest.jpg",
+    "title": "The Great Breakup Contest"
+},
+{
+    "link": "https://vimeo.com/109199029",
+    "image": "the_halloween_costume.jpeg",
+    "title": "The Halloween Costume"
+},
+{
+    "link": "https://vimeo.com/75525905",
+    "image": "the_last_templar.jpeg",
+    "title": "The Last Templar"
+},
+{
+    "link": "https://vimeo.com/178542768",
+    "image": "the_wedding_party_trailer.jpeg",
+    "title": "The Wedding Party - Trailer"
+},
+{
+    "link": "https://vimeo.com/88110576",
+    "image": "travis_hayes_bath_salts.jpeg",
+    "title": "Travis Hayes - Bath Salts"
+},
+{
+    "link": "https://vimeo.com/107635172",
+    "image": "travis_hayes_teen_haze.jpeg",
+    "title": "Travis Hayes - Teen Haze"
+},
+{
+    "link": "https://vimeo.com/134135617",
+    "image": "understanding_healthcare.jpeg",
+    "title": "Understanding Healthcare"
+}
+
+] // don't touch this line
